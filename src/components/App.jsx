2331a0bb659f00f2844c3145a21bc2e207c70c6d
Veronika Sirak <7nicka@gmail.com>
2022-05-30 00:36:21 +0100

    add components and data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+
+import Profile from './Profile/Profile';
+import user from './data/user.json';
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return 
 };
+
